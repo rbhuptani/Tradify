d294dc05a037fbd1657ea4a88aa064eefeb4b835
ronakbhuptani <ronakbhuptani@hotmail.com>
2016-04-11 16:37:57 -0400

    Initial Version

---
+++ app/src/main/java/com/hello_world/ronak/tradify/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hello_world.ronak.tradify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
