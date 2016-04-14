8d8568ab1fab25e63e18fcc13785e98ab200cd87
ronakbhuptani <ronakbhuptani@hotmail.com>
2016-04-14 17:32:58 -0400

    stores user data

---
+++ app/src/main/java/com/hello_world/ronak/tradify/UserContext.java
@@ -0,0 +1,11 @@
+package com.hello_world.ronak.tradify;
+
+/**
+ * Created by ronak_000 on 4/14/2016.
+ */
+public class UserContext {
+    static String USERID = "";
+    static String USEREMAIL = "";
+    static String USERNAME="";
+    static String USERPROFILEURL="";
+}
